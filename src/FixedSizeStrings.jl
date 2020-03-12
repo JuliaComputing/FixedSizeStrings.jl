0615707181fe48ba1f28735280003b1f450f5225
Fabian Gans <fgans@bgc-jena.mpg.de>
2020-03-12 16:31:20 +0100

    Add parameter to string to support U16 and U32

--- src/FixedSizeStrings.jl
+++ src/FixedSizeStrings.jl
@@ -1,41 +1,48 @@
 module FixedSizeStrings
 
-export FixedSizeString
+export FixedSizeString, FixedSizeString16, FixedSizeString32
 
 import Base: iterate, lastindex, getindex, sizeof, length, ncodeunits, codeunit, isvalid, read, write
 
-struct FixedSizeString{N} <: AbstractString
-    data::NTuple{N,UInt8}
-    FixedSizeString{N}(itr) where {N} = new(NTuple{N,UInt8}(itr))
+struct FixedSizeStringGeneric{N,T} <: AbstractString
+    data::NTuple{N,T}
+    FixedSizeStringGeneric{N,T}(itr) where {N,T} = new(NTuple{N,T}(itr))
 end
 
-FixedSizeString(s::AbstractString) = FixedSizeString{length(s)}(s)
+const FixedSizeString{N} = FixedSizeStringGeneric{N,UInt8} where N
+const FixedSizeString16{N} = FixedSizeStringGeneric{N,UInt16} where N
+const FixedSizeString32{N} = FixedSizeStringGeneric{N,UInt32} where N
 
-function iterate(s::FixedSizeString{N}, i::Int = 1) where N
+FixedSizeString(s::AbstractString) = FixedSizeString{length(s)}(s)
+FixedSizeString16(s::AbstractString) = FixedSizeString16{length(s)}(s)
+FixedSizeString32(s::AbstractString) = FixedSizeString32{length(s)}(s)
+
+function iterate(s::FixedSizeStringGeneric{N}, i::Int = 1) where N
     i > N && return nothing
     return (Char(s.data[i]), i+1)
 end
 
-lastindex(s::FixedSizeString{N}) where {N} = N
+lastindex(s::FixedSizeStringGeneric{N}) where {N} = N
 
-getindex(s::FixedSizeString, i::Int) = Char(s.data[i])
+getindex(s::FixedSizeStringGeneric, i::Int) = Char(s.data[i])
 
-sizeof(s::FixedSizeString) = sizeof(s.data)
+sizeof(s::FixedSizeStringGeneric) = sizeof(s.data)
 
-length(s::FixedSizeString) = length(s.data)
+length(s::FixedSizeStringGeneric) = length(s.data)
 
-ncodeunits(s::FixedSizeString) = length(s.data)
+ncodeunits(s::FixedSizeStringGeneric) = length(s.data)
 
-codeunit(::FixedSizeString) = UInt8
-codeunit(s::FixedSizeString, i::Integer) = s.data[i]
+codeunit(::FixedSizeStringGeneric{<:Any,T}) where T = T
+codeunit(s::FixedSizeStringGeneric, i::Integer) = s.data[i]
 
 isvalid(s::FixedSizeString, i::Int) = checkbounds(Bool, s, i)
+isvalid(s::FixedSizeStringGeneric, i::Int) = checkbounds(Bool, s, i) && isvalid(Char,s.data[i])
 
-function read(io::IO, T::Type{FixedSizeString{N}}) where N
+function read(io::IO, T::Type{<:FixedSizeStringGeneric{N}}) where N
     return read!(io, Ref{T}())[]::T
 end
 
-function write(io::IO, s::FixedSizeString{N}) where N
+function write(io::IO, s::FixedSizeStringGeneric{N}) where N
     return write(io, Ref(s))
 end
 
